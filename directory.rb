1ef42090885261657f23bb1fede84138436196f6
Jonah Massam <Jonah.Massam10@yahoo.co.uk>
2022-01-13 12:15:35 +0000

    cmd line and fileload

--- directory.rb
+++ directory.rb
@@ -67,14 +67,27 @@ def save_students
   file.close
 end
 
-def load_students
-  file = File.open("students.csv", "r")
+def load_students(file_name = "students.csv")
+  file = File.open(file_name, "r")
   for line in file.readlines
     name, cohort = line.chomp.split(",")
     @students << {name: name, cohort: cohort.to_sym}
   end
   file.close
 end
+
+def try_to_load_students
+  filename = ARGV.first
+  return if filename.nul?
+  if File.exists?(filename)
+    load_students(filename)
+    puts "Loaded #{@students.count} from #{filename}"
+  else
+    puts "File #{filename} dosen't exist"
+    exit
+  end
+end
+
 
 def interactive_menu
   loop do
